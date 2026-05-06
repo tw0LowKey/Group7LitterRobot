e2b737d779f951e2af4154c15646618c6d7f8452
SeanF1ynn <159140611+SeanF1ynn@users.noreply.github.com>
2026-05-06 12:26:38 +0100

    Update setup.py

--- scout_ws1/src/waypoint_navigation_pkg/setup.py
+++ scout_ws1/src/waypoint_navigation_pkg/setup.py
@@ -13,6 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('waypoint_navigation_pkg/*.yaml')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
             'waypoint_navigation_node = waypoint_navigation_pkg.waypoint_navigation_node:main',
-        ],
+            'gps_follower_behaviour_node = waypoint_navigation_pkg.gps_follower_behaviour_node:main',
+                    ],
     },
 )
