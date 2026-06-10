9620146f62bd6c125b190d6325cf2ee19e517963
Thierry Popat <Thierry_popat@hotmail.com>
2026-06-10 01:50:51 +0100

    Fix the emergency stop button node

--- sapling_ws/src/misc/misc/sapling_emergency_stop_node.py
+++ sapling_ws/src/misc/misc/sapling_emergency_stop_node.py
@@ -2,6 +2,7 @@ import board
 import digitalio
 import rclpy
 import subprocess
+from collections import deque
 from os import environ
 from rclpy.node import Node
 from std_srvs.srv import SetBool
@@ -15,23 +16,36 @@ class EmergencyStopNode(Node):
 		self.stopped = False
 		self.physicalStopButtonVal = False
 		self.virtualStopButtonVal = False
+		self.lastPhysicalStopButtonVal = False
+		self.physicalStopButtonQueue = deque(maxlen=30)
+		self.physicalStopButtonTriggerThreshold = 12
 
 		# Pins
-		self.physicalStopButton = digitalio.DigitalInOut(board.D13)
+		self.physicalStopButton = digitalio.DigitalInOut(board.D19)
 		self.physicalStopButton.direction = digitalio.Direction.INPUT
 
 		# Services
 		self.virtualStopButtonService = self.create_service(SetBool, "/comms/virtual_emergency_stop", self.virtualStopButtonServiceCallback)
 
 		# Timers
-		self.checkPhysicalEmergencyStopButtonTimer = self.create_timer(0.05, self.checkPhysicalEmergencyStopButton)
+		self.checkPhysicalEmergencyStopButtonTimer = self.create_timer(0.01, self.checkPhysicalEmergencyStopButton)
 		self.checkStopButtonsTimer = self.create_timer(0.1, self.checkStopButtons)
 
 		self.get_logger().info("Emergency Stop Node Started")
 
 	def checkPhysicalEmergencyStopButton(self):
-		self.physicalStopButtonVal = not self.physicalStopButton.value
-		self.get_logger().debug(f"The value of the phsyical stop button is {self.physicalStopButtonVal}")
+		self.physicalStopButtonQueue.append(not self.physicalStopButton.value)
+
+		currentPhysicalStopButtonVal = self.physicalStopButtonQueue.count(True) > self.physicalStopButtonTriggerThreshold
+
+		if currentPhysicalStopButtonVal != self.lastPhysicalStopButtonVal:
+			self.physicalStopButtonVal = currentPhysicalStopButtonVal
+			self.lastPhysicalStopButtonVal = currentPhysicalStopButtonVal
+
+			if self.physicalStopButtonVal:
+				self.get_logger().warn("The physical emergency stop button has been pressed")
+			else:
+				self.get_logger().warn("The physical emergency stop button has been released")
 
 	def virtualStopButtonServiceCallback(self, request, response):
 		self.virtualStopButtonVal = request.data
@@ -39,44 +53,42 @@ class EmergencyStopNode(Node):
 		response.success = True
 		response.message = f"Virtual emergency stop set to {request.data}"
 
+		if self.virtualStopButtonVal:
+			self.get_logger().warn("The virtual emergency stop button has been pressed")
+		else:
+			self.get_logger().warn("The virtual emergency stop button has been released")
+
 		return response
 
 	def checkStopButtons(self):
 		if self.physicalStopButtonVal or self.virtualStopButtonVal:
 			if not self.stopped:
-				self.enableEmergencyStop(self.virtualStopButtonVal)
+				self.enableEmergencyStop()
 		else:
 			if self.stopped:
 				self.disableEmergencyStop()
 
-	def enableEmergencyStop(self, virtual: bool):
+	def enableEmergencyStop(self):
 		try:
 			subprocess.run(["sudo", "-n", "ip", "link", "set", "can_scout", "down"], check=True)
 			if self.SAPLING_ROLE == "pickbot":
 				subprocess.run(["sudo", "-n", "ip", "link", "set", "can_arm", "down"], check=True)
 
 			self.stopped = True
+
 		except subprocess.CalledProcessError as e:
 			self.get_logger().error(f"Failed to bring the CAN interface down: {e}")
 
-			return
-
-
-		if virtual:
-			self.get_logger().warn("The virtual emergency stop button has been pressed")
-		else:
-			self.get_logger().warn("The physical emergency stop button has been pressed")
-
 	def disableEmergencyStop(self):
 		try:
-			subprocess.run(["sudo", "-n", "ip", "link", "set", "can_scout", "up"], check=True)
+			subprocess.run(["sudo", "-n", "ip", "link", "set", "can_scout", "up", "type", "can", "bitrate", "500000"], check=True)
 			if self.SAPLING_ROLE == "pickbot":
-				subprocess.run(["sudo", "-n", "ip", "link", "set", "can_arm", "up"], check=True)
+				subprocess.run(["sudo", "-n", "ip", "link", "set", "can_arm", "up", "type", "can", "bitrate", "1000000", "sample-point", "0.875"], check=True)
 
 			self.stopped = False
+
 		except subprocess.CalledProcessError as e:
 			self.get_logger().error(f"Failed to bring the CAN interface up: {e}")
-
 
 def main(args=None):
 	rclpy.init(args=args)
@@ -87,9 +99,9 @@ def main(args=None):
 	except KeyboardInterrupt:
 		pass
 	finally:
-		emergencyStopNode.destroy_node()
 		if rclpy.ok():
 			rclpy.shutdown()
+		emergencyStopNode.destroy_node()
 
 if __name__ == "__main__":
 	main()
